f13bf788f605c9772b4732db4ae5fc6b3877b65c
Saptarshi-26 <saptarshiok26@gmail.com>
2024-10-01 11:11:14 +0530

    added second largest

---
+++ Array/src/SecondLargest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of the array");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        int t=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]<t)t=arr[i];
+        }
+        int tt=t;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]>t){
+                tt=t;
+                t=arr[i];
+            }
+            else if(arr[i]>tt&&arr[i]!=t){
+                tt=arr[i];
+            }
+        }
+        System.out.println(tt);
+    }
+}
