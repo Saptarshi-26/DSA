94578d5b34172da5df6bf3be0263ef1ec9dfbfb5
Saptarshi-26 <saptarshiok26@gmail.com>
2024-10-01 11:20:42 +0530

    updated

--- Array/src/SecondLargest.java
+++ Array/src/SecondLargest.java
@@ -6,6 +6,7 @@ public class SecondLargest {
         System.out.println("enter the length of the array");
         int n = sc.nextInt();
         int arr[]=new int[n];
+        System.out.println("enter the element in the array ");
         for(int i=0;i<n;i++){
             arr[i]= sc.nextInt();
         }
