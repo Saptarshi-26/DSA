330a97aadc9a6c62fe62eac488f3e1d8ae11a6a1
Saptarshi-26 <saptarshiok26@gmail.com>
2026-03-18 11:36:40 +0530

    completed submission on leetcode

--- Dsa_Learn_and_Practice/src/Minimum_Window_Substring.java
+++ Dsa_Learn_and_Practice/src/Minimum_Window_Substring.java
@@ -6,42 +6,45 @@ public class Minimum_Window_Substring {
 
     public static String minWindow(String s, String t) {
 
-        HashMap<Character, Long> char_freq = (HashMap<Character, Long>) t.chars().
-                mapToObj(x -> (char) x).
-                collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+        if (s.length() < t.length()) return "";
+        HashMap<Character, Long> char_freq = t.chars().mapToObj(x -> (char) x).collect(Collectors.groupingBy(x -> x, HashMap::new, Collectors.counting()));
 
         int count = 0;
 
         int i = -1;
-        while (!char_freq.containsKey(s.charAt(++i)));
-        int j = i+1;
-        String ans = s;
-        while (j < s.length()) {
+        while ((i + 1) < s.length() && !char_freq.containsKey(s.charAt(++i))) ;
+        int j = i;
+        String ans = s + s;
+        while (i < s.length() && j < s.length()) {
             char ci = s.charAt(i);
             char cj = s.charAt(j);
-            if (!char_freq.containsKey(ci)) {
-                i++;
-                continue;
-            }
-            if (count == char_freq.size()) {
-                if (char_freq.containsKey(ci)) {
-                    char_freq.put(ci, char_freq.get(ci) + 1);
-                    if (char_freq.get(ci) > 0) count--;
+            if (char_freq.containsKey(cj)) {
+                if (!char_freq.containsKey(ci)) {
+                    i++;
+                    if (i >= s.length()) break;
+                    if (char_freq.containsKey(s.charAt(i)) && count != char_freq.size()) j++;
+                    continue;
                 }
-                if(count==char_freq.size())i++;
-            } else {
-                if (char_freq.containsKey(cj)) {
+                if (count == char_freq.size()) {
+                    String temp = s.substring(i, j + 1);
+                    ans = temp.length() < ans.length() ? temp : ans;
+                    char_freq.put(ci, char_freq.get(ci) + 1);
+                    i++;
+                    if (char_freq.get(ci) > 0) count--;
+
+                    if (i >= j || char_freq.containsKey(s.charAt(i))) {
+                        if (count < char_freq.size()) {
+                            j++;
+                        }
+                    }
+                } else {
                     char_freq.put(cj, char_freq.get(cj) - 1);
                     if (char_freq.get(cj) == 0) count++;
+                    if (count != char_freq.size()) j++;
                 }
-                if (count != char_freq.size()) j++;
-                else {
-                    String b = s.substring(i, j + 1);
-                    ans = b.length() < ans.length() ? b : ans;
-                }
-
-            }
+            } else j++;
         }
+        if (ans.length() > s.length()) ans = "";
 
         return ans;
 
