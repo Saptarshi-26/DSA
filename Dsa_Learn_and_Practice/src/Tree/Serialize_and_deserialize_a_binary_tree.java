f47f0da8092ea7d62cf368742d485bfdace15b90
Saptarshi-26 <saptarshiok26@gmail.com>
2026-03-25 13:30:18 +0530

    started serialize and deserialize binary tree problem

---
+++ Dsa_Learn_and_Practice/src/Tree/Serialize_and_deserialize_a_binary_tree.java
@@ -0,0 +1,41 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serialize_and_deserialize_a_binary_tree {
+    // Encodes a tree to a single string.
+    public String serialize(Node root) {
+        if(root==null)return "null";
+        String ans =Integer.toString(root.data);
+        ArrayList<Node> node = new ArrayList<>(List.of(root));
+        while(!node.isEmpty()){
+            ArrayList<Node> node1 = new ArrayList<>();
+            for(Node x : node){
+                if(x.left!=null){
+                    ans+=" "+Integer.toString(x.left.data);
+                    node1.add(x.left);
+                }
+                else ans+=" "+"null";
+                if(x.right!=null){
+                    ans+=" "+Integer.toString(x.right.data);
+                    node1.add(x.right);
+                }
+                else ans+=" "+"null";
+
+            }
+            node = node1;
+        }
+        return ans;
+
+    }
+
+    // Decodes your encoded data to tree.
+    public Node deserialize(String data) {
+        String[] node = data.split(" ");
+        int i=0;
+        int node_per_layer=1;
+        return null;
+
+    }
+}
